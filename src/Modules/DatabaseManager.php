99970b0fd162842bef7f3fb5744a45b0a0af84a9
David Kim <woody@woody.co.kr>
2024-02-29 19:50:09 +0900

    Update 1.0.1
    Update 1.0.1

--- src/Modules/DatabaseManager.php
+++ src/Modules/DatabaseManager.php
@@ -4,7 +4,7 @@ use PazerCore\Structs\DatabaseConfig;
 class DatabaseManager {
     protected array $_config;
     public function __construct() { $this->_config = array(); return $this; }
-    public function setClient(string $name, string $hostname, string $username, string $password, string $database, string $charset = "utf8mb4", int $port = 3306) : self {
+    public function setClient(string $name, string $hostname, string $username, string $password, string $database, int $port = 3306, string $charset = "utf8mb4") : self {
         $server = new DatabaseConfig();
         $server->hostname = $hostname;
         $server->username = $username;
@@ -13,6 +13,10 @@ class DatabaseManager {
         $server->port = $port ?? 3306;
         $server->charset = $charset;
         $this->_config[$name] = $server;
+        return $this;
+    }
+    public function setClientForm(string $name, DatabaseConfig $config) : self {
+        $this->_config[$name] = $config;
         return $this;
     }
     public function getClient(string $name) : ?DatabaseClient { return new DatabaseClient($this->_config[$name]) ?? null; }
--- src/PazerCore.php
+++ src/PazerCore.php
@@ -4,24 +4,20 @@ use PazerCore\Modules\ApiManager;
 use PazerCore\Modules\ConfigManager;
 use PazerCore\Modules\DatabaseManager;
 use PazerCore\Modules\RouteManager;
-use PazerCore\Modules\Router;
 class PazerCore {
     protected DatabaseManager $_dbms;
     protected ConfigManager $_config;
     protected ApiManager $_api;
     protected RouteManager $_route;
-    protected Router $_router;
     public function __construct() {
         $this->_config = new ConfigManager();
         $this->_dbms = new DatabaseManager();
         $this->_api = new ApiManager($this->_config);
         $this->_route = new RouteManager($this->_config);
-        $this->_router = new Router($this->_config);
         return $this;
     }
     public function config() : ConfigManager { return $this->_config; }
     public function dbms() : DatabaseManager { return $this->_dbms; }
     public function api() : ApiManager { return $this->_api; }
     public function route() : RouteManager { return $this->_route; }
-    public function router() : Router { return $this->_router; }
 }
